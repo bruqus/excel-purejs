9061da902e161cd5d55c7e853ceb8670162a4361
Nikita <bruqusg@gmail.com>
2020-07-02 23:27:04 +0500

    finish base framework functional

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -5,6 +5,13 @@ import { ExcelComponent } from '@core/ExcelComponent';
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
 
+  constructor($root, options) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click'],
+    });
+  }
+
   toHTML() {
     return `
       <div class="button">
@@ -26,5 +33,9 @@ export class Toolbar extends ExcelComponent {
         <i class="material-icons">format_underline</i>
       </div>
     `;
+  }
+
+  onClick(event) {
+    console.log(event.target);
   }
 }
