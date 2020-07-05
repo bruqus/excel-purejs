64c055af0dbb842906e4441d4b2b10e82fa0ff94
Nikita <bruqusg@gmail.com>
2020-07-05 22:44:40 +0500

    finish resize functionality

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,15 +5,15 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column" data-type="resizable">
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -26,7 +26,7 @@ function createRow(index, content) {
       : '';
 
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
         ${resize}
