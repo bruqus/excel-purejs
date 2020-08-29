dae8d3c8999a25c2517a1ebe9065c089110e5bc7
Nikita Bruqus <bruqusg@gmail.com>
2020-08-29 23:37:37 +0500

    feat: added dynamic creation of toolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,6 +1,8 @@
 'use strict';
 
+import { $ } from '@core/dom';
 import { ExcelComponent } from '@core/ExcelComponent';
+import { createToolbar } from '@/components/toolbar/toolbar.template';
 
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
@@ -8,30 +10,19 @@ export class Toolbar extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
+      listeners: ['click'],
       ...options,
     });
   }
 
   toHTML() {
-    return `
-      <div class="button">
-        <i class="material-icons">format_align_left</i>
-      </div>
-      <div class="button">
-        <i class="material-icons">format_align_center</i>
-      </div>
-      <div class="button">
-        <i class="material-icons">format_align_right</i>
-      </div>
-      <div class="button">
-        <i class="material-icons">format_bold</i>
-      </div>
-      <div class="button">
-        <i class="material-icons">format_italic</i>
-      </div>
-      <div class="button">
-        <i class="material-icons">format_underline</i>
-      </div>
-    `;
+    return createToolbar();
+  }
+
+  onClick(event) {
+    const $target = $(event.target);
+    if ($target.data.type === 'button') {
+      console.log($target.data.value);
+    }
   }
 }
