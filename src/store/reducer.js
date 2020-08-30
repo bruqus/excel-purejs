c08e167d035d8461d71e3cfee9b7d4d225ec05d5
Nikita Bruqus <bruqusg@gmail.com>
2020-08-30 23:24:35 +0500

    refactor: refactor reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,19 +4,25 @@ import { TABLE_RESIZE, CHANGE_TEXT } from '@/store/types';
 
 export function reducer(state, action) {
   let field;
-  let prevState;
 
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'col' ? 'colState' : 'rowState';
-      prevState = state[field] || {};
-      prevState[action.data.id] = action.data.value;
-      return { ...state, [field]: prevState };
+      return { ...state, [field]: value(state, field, action) };
     case CHANGE_TEXT:
-      prevState = state['dataState'] || {};
-      prevState[action.data.id] = action.data.value;
-      return { ...state, currentText: action.data.value, dataState: prevState };
+      field = 'dataState';
+      return {
+        ...state,
+        currentText: action.data.value,
+        dataState: value(state, field, action),
+      };
     default:
       return state;
   }
+}
+
+function value(state, field, action) {
+  const val = state[field] || {};
+  val[action.data.id] = action.data.value;
+  return val;
 }
