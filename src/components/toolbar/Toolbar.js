7f00cde6ade43a4297dcc965edb654e423975ffe
Nikita Bruqus <bruqusg@gmail.com>
2020-08-30 22:28:47 +0500

    feat: added ExcelStateComponent

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,10 +1,10 @@
 'use strict';
 
 import { $ } from '@core/dom';
-import { ExcelComponent } from '@core/ExcelComponent';
+import { ExcelStateComponent } from '@core/ExcelStateComponent';
 import { createToolbar } from '@/components/toolbar/toolbar.template';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = 'excel__toolbar';
 
   constructor($root, options) {
@@ -15,14 +15,31 @@ export class Toolbar extends ExcelComponent {
     });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      textDecoration: 'none',
+      fontStyle: 'normal',
+    };
+
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return createToolbar();
+    return this.template;
   }
 
   onClick(event) {
     const $target = $(event.target);
     if ($target.data.type === 'button') {
-      console.log($target.data.value);
+      const value = JSON.parse($target.data.value);
+      const key = Object.keys(value)[0];
+      this.setState({ [key]: value[key] });
     }
   }
 }
