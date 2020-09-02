61fd1697eee40464b155a7a0bc5eec9a45ef9cda
Nikita Bruqus <bruqusg@gmail.com>
2020-09-02 23:08:45 +0500

    feat: added page navigation when changing url

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { $ } from '@core/dom';
-// import { ActiveRoute } from '@core/routes/ActiveRoute';
+import { ActiveRoute } from '@core/routes/ActiveRoute';
 
 export class Router {
   constructor(selector, routes) {
@@ -11,6 +11,7 @@ export class Router {
 
     this.$placeholder = $(selector);
     this.routes = routes;
+    this.page = null;
 
     this.changePageHandler = this.changePageHandler.bind(this);
 
@@ -23,11 +24,18 @@ export class Router {
   }
 
   changePageHandler() {
-    const Page = this.routes.excel;
-    const page = new Page();
-    this.$placeholder.append(page.getRoot());
+    if (this.page) {
+      this.page.destroy();
+    }
+    this.$placeholder.clear();
 
-    page.afterRender();
+    const Page = ActiveRoute.path.includes('excel')
+      ? this.routes.excel
+      : this.routes.dashboard;
+
+    this.page = new Page(ActiveRoute.param);
+    this.$placeholder.append(this.page.getRoot());
+    this.page.afterRender();
   }
 
   destroy() {
